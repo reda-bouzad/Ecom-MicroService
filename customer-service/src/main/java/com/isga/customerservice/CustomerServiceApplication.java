8162fd7ca03c0c49c5ef58a81504b63e50b257c0
Reda-Bouzad <redabouzadfst@gmail.com>
2025-02-15 11:41:28 +0100

    config-repo-2

--- customer-service/src/main/java/com/isga/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/isga/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,16 @@
 package com.isga.customerservice;
 
+import com.isga.customerservice.config.CustomerConfigParams;
 import com.isga.customerservice.entities.Customer;
 import com.isga.customerservice.repositories.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
---
+++ customer-service/src/main/java/com/isga/customerservice/config/CustomerConfigParams.java
@@ -0,0 +1,7 @@
+package com.isga.customerservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "customer.params")
+public record CustomerConfigParams(int x, int y) {
+}
